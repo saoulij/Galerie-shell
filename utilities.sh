e8d9cfba6d52e958d5bb11fdf0c947a2fc009670
Julie Saouli <Julie.Saouli@grenoble-inp.org>
2016-12-05 20:17:52 +0100

    echo -> redirections entrée/sortie

--- utilities.sh
+++ utilities.sh
@@ -1,57 +1,62 @@
 #! /bin/sh
 
 html_head () {
-    echo "<!DOCTYPE html>"
-    echo ""
-    echo "<html>"
-    echo "    <head>"
-    echo "        <meta charset=utf-8 />"
-    echo "        <title>$1</title>"
-    echo "    </head>"
-    echo ""
-    echo "    <body>"
-    echo ""
+cat << EOF > "$1"
+<!DOCTYPE html>
+  <html>
+      <head>
+          <meta charset=utf-8 />
+          <title>$2</title>
+      </head>
+
+      <body>
+
+EOF
 }
 
 html_title () {
-    echo "        <h1>$1</h1>"
-    echo ""
+cat << EOF >> "$1"
+       <h1>$2</h1>
+
+EOF
 }
 
 html_tail () {
-    echo "    </body>"
-    echo "</html>"
+cat << EOF >> "$1"
+    </body>
+</html>
+EOF
 }
 
 generate_img_fragment () {
-    echo "        <img src=$1 alt=$(basename -s .jpg $1)>"
-    echo ""
+cat << EOF >> "$1"
+        <img src=$2 alt=$(basename -s .jpg $2)>
+
+EOF
 }
 
 generate_galerie () {
-    for img in $(ls "$1/"*.jpg);
+    for img in "$1/"*.jpg;
     do
       if [ -r "$img" ];
       then
-        if [ ! -d "$2" ];
+        if [ ! -d "$2/vignette" ];
           then
-            mkdir "$2"
+            mkdir "$2/vignette"
           fi
-          vignette="$2/$(basename $img)"
-          if [ ! -f "$2/$img" ] || [ "$3" ];
+          vignette="$2/vignette/$(basename $img)"
+          if [ ! -f "$vignette" ] || [ "$3" -eq 1 ];
           then
             gmic "$img" -cubism , -resize 200,200 -output "$vignette"
           fi
-        generate_img_fragment "$vignette"
+        generate_img_fragment "$4" "$vignette"
       fi
     done
 }
 
 galerie_main () {
-  (
-  html_head "Test de la génération de HTML"
-  html_title 'Galerie HTML'
-  generate_galerie "$1" "$2" "$3"
-  html_tail
-  ) > "$4"
+  html_head "$4" "Test de la génération de HTML"
+  html_title "$4" "Galerie HTML"
+  generate_galerie "$1" "$2" "$3" "$4"
+  html_tail "$4"
 }
