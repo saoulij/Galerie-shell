d1211d54226b8aedad900ca5c8dda09ce59b6997
Julie Saouli <Julie.Saouli@grenoble-inp.org>
2016-12-06 17:04:24 +0100

    generation pages individuelles pour images source

--- utilities.sh
+++ utilities.sh
@@ -8,37 +8,9 @@ cat << EOF > "$1"
           <meta charset=utf-8 />
           <title>$2</title>
           <style>
-          body {
-              background-color: #DCDCDC;
-              font-size: 100%;
-          }
-
-          h1 {
-            text-align: center;
-            font-size: 3em;
-            font-family: Verdana, Sans-Serif;
-          }
-
-          .imageframe {
-            float: left;
-            background-color: white;
-            border: 1px dashed #C0C0C0;
-            padding: 8px;
-            margin: 8px;
-            text-align:center;
-          }
-
-          .image {
-            border: 1px solid black;
-            border-radius: 5px;
-          }
-
-          .legend {
-            font-style: italic;
-            font-family: "Times New Roman", Serif;
-            font-size: 1.5em;
-            text-transform: capitalize;
-          }
+EOF
+cat "$3" >> "$1"
+cat << EOF >> "$1"
           </style>
       </head>
 
@@ -64,11 +36,30 @@ generate_img_fragment () {
 name="$(basename -s .jpg "$2")"
 cat << EOF >> "$1"
         <div class="imageframe">
-        <img class="image" src=$2 alt=$name><br>
+        <a href="$3/$name.html">
+        <img class="image" src=$2 alt=$name>
+        </a><br>
         <span class="legend">$name</span>
         </div>
 
 EOF
+}
+
+generate_img_html () {
+  pagename=""$2"/"$3".html"
+  html_head "$pagename" "$3" "image_style.css"
+  html_title "$pagename" "$3"
+cat << EOF >> "$pagename"
+        <div class="imageframe">
+        <img class="image" src=$1/$3.jpg alt=$3><br>
+        </div>
+
+        <ul class="navbar">
+        <li><a href="$4">Retour à la galerie</a></li>
+        </ul>
+
+EOF
+  html_tail "$pagename"
 }
 
 generate_galerie () {
@@ -76,22 +67,23 @@ generate_galerie () {
     do
       if [ -r "$img" ];
       then
-        if [ ! -d "$2/vignette" ];
+        if [ ! -d ""$2"/vignette" ];
           then
-            mkdir "$2/vignette"
+            mkdir ""$2"/vignette"
           fi
-          vignette="$2/vignette/$(basename "$img")"
+          vignette=""$2"/vignette/$(basename "$img")"
           if [ ! -f "$vignette" ] || [ "$3" -eq 1 ];
           then
             gmic "$img" -cubism , -resize 200,200 -output "$vignette"
           fi
-        generate_img_fragment "$4" "$vignette"
+        generate_img_fragment "$4" "$vignette" "$2"
+        generate_img_html "$1" "$2" "$(basename -s .jpg "$vignette")" "$4"
       fi
     done
 }
 
 galerie_main () {
-  html_head "$4" "Galerie HTML"
+  html_head "$4" "Galerie HTML" "galerie_style.css"
   html_title "$4" "Galerie d'images"
   generate_galerie "$1" "$2" "$3" "$4"
   html_tail "$4"
