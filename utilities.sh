2aec7c5a841d1d25dc2c0ff5b63382487e67ea20
Julie Saouli <Julie.Saouli@grenoble-inp.org>
2016-12-15 19:33:31 +0100

    mode verbeux

--- utilities.sh
+++ utilities.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 html_head () {
+  if [ "$VERB" -eq 1 ]; then echo "creation de $(basename "$1") et ajout de l'entete"; fi
+
   cat << EOF > "$1"
 <!DOCTYPE html>
   <html>
@@ -19,6 +21,8 @@ EOF
 }
 
 html_title () {
+  if [ "$VERB" -eq 1 ]; then echo "ajout d'un titre a $(basename "$1")"; fi
+
   cat << EOF >> "$1"
        <h1>$2</h1>
 
@@ -26,6 +30,8 @@ EOF
 }
 
 html_tail () {
+  if [ "$VERB" -eq 1 ]; then echo "ajout du code fin a $(basename "$1")"; fi
+
   cat << EOF >> "$1"
       </body>
   </html>
@@ -33,6 +39,8 @@ EOF
 }
 
 generate_img_fragment () {
+  if [ "$VERB" -eq 1 ]; then echo "ajout de la vignette $(basename "$vignette") a $(basename "$INDEX")"; fi
+
   cat << EOF >> "$INDEX"
         <div class="frame">
         <div class="imageframe">
@@ -46,6 +54,8 @@ EOF
 }
 
 generate_navi () {
+  if [ "$VERB" -eq 1 ]; then echo "generation de la navigation sur la page $(basename "$1")"; fi
+
   if [ $i -ne 0 ]; then
     p=$(($i-1))
     prec="$DEST/$(basename -s .jpg "${IMG[p]}").html"
@@ -70,9 +80,12 @@ EOF
 }
 
 generate_img_html () {
+  if [ "$VERB" -eq 1 ]; then echo "generation de la page $(basename "$1")"; fi
+
   html_head "$1" "$imgname" "image_style.css"
   html_title "$1" "$imgname"
 
+  if [ "$VERB" -eq 1 ]; then echo "ajout de $imgname a $(basename "$1")"; fi
   cat << EOF >> "$1"
         <div class="imageframe">
         <img class="image" src=$SOURCE/$imgname.jpg alt=$imgname><br>
@@ -90,6 +103,7 @@ generate_galerie () {
     if [ ! -d "$DEST/vignette" ];
     then
       mkdir "$DEST/vignette"
+      if [ "$VERB" -eq 1 ]; then echo "mkdir: creation du repertoire vignette"; fi
     fi
 
     for i in "${!IMG[@]}";
@@ -101,6 +115,7 @@ generate_galerie () {
           if [ ! -f "$vignette" ] || [ "$FORCE" -eq 1 ];
           then
             gmic "${IMG[i]}" -cubism , -resize 200,200 -output "$vignette"
+            if [ "$VERB" -eq 1 ]; then echo "creation de la vignette de l'image $imgname"; fi
           fi
 
       generate_img_fragment
@@ -111,10 +126,6 @@ generate_galerie () {
 }
 
 galerie_main () {
-  SOURCE="$1"
-  DEST="$2"
-  FORCE="$3"
-  INDEX="$4"
   IMG=("$SOURCE"/*.jpg)
 
   html_head "$INDEX" "Galerie HTML" "galerie_style.css"
